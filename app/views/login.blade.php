cda6af6282aa4564445b797e603fad19d4e88e89
Arran Jacques <arranjacques@gmail.com>
2014-05-25 17:15:32 +0100

    Updates styling for dashboard login page.

--- app/views/login.blade.php
+++ app/views/login.blade.php
@@ -4,21 +4,25 @@
 		<div class="login__box">
 			{{ Form::open(array('class' => 'login__form')) }}
 				@if ($messages)
-					<div class="message_box message_box--tomato">
-						<ul>
-							@foreach($messages->all() as $message)
-								<li>{{ $message }}</li>
-							@endforeach
-						</ul>
-					</div> 
+					<div class="node__y--bottom">
+						<div class="message_box message_box--tomato">
+							<ul>
+								@foreach($messages->all() as $message)
+									<li>{{ $message }}</li>
+								@endforeach
+							</ul>
+						</div>
+					</div>
 				@endif
 				<div class="block__y__bottom">
 					{{ Form::label('email', 'Email', array('class' => 'label label--block')) }}
 					{{ Form::input('email', 'email', Input::has('email') ? Input::get('email') : null, array('class' => 'input__text')) }}
+				</div>
+				<div class="block__y__bottom"> 
 					{{ Form::label('password', 'Password', array('class' => 'label label--block')) }}
 					{{ Form::input('password', 'password', null, array('class' => 'input__text')) }}
 				</div>
-				<div class="block__y__top">
+				<div>
 					{{ Form::submit('Sign In', array('class' => 'button button--teal')) }}
 					<label for="remember_me" class="label checkbox login__form__remember">
 						<input type="checkbox" name="remember_me" value="1" {{ Input::has('remember_me') ? 'checked="checked"' : '' }} class="checkbox__input" id="remember_me"/>
